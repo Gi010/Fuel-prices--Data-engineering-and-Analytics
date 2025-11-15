a359be966156210d74b474def14fef4d35aad9e9
George Pagava <133568900+Gi010@users.noreply.github.com>
2025-11-15 13:23:16 +0400

    Add files via upload
    The script triggers the stored procedure in MS SQL Server which loads the latest oil price data into the data warehouse

---
+++ scripts/brent_oli_sql_exec_stored_procedure.py
@@ -0,0 +1,31 @@
+import pyodbc
+
+# Connection settings
+server = r"GPAGHAVA\GPAGAVA"
+database = "OilDataWarehouse"
+
+# Create connection using Windows Authentication
+conn = pyodbc.connect(
+    rf"DRIVER={{ODBC Driver 17 for SQL Server}};"
+    rf"SERVER={server};"
+    rf"DATABASE={database};"
+    rf"Trusted_Connection=yes;"
+)
+
+cursor = conn.cursor()
+
+try:
+    # Execute the stored procedure
+    cursor.execute("EXEC bronze.load_brent_oil")
+
+    # Commit if your stored procedure does INSERT/UPDATE/DELETE
+    conn.commit()
+
+    print("Stored procedure executed successfully.")
+
+except Exception as e:
+    print("Error executing stored procedure:", e)
+
+finally:
+    cursor.close()
+    conn.close()
